e08086aa5183b1e88f8764100d114dfe62224bf8
nintichance <mintidance@gmail.com>
2018-05-04 17:31:45 -0400

    Created function which uses the map method to iterate through the array of contacts and display in the the dumb component Contact

--- client/src/components/ContactsList.js
+++ client/src/components/ContactsList.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react'
-
+import Contact from './Contact'
 class ContactsList extends Component {
     componentWillMount(){
         this.props.getContacts()
     }
+  
     render(){
+        const contactsList = this.props.contacts.map((contact)=>{
+            console.log(contact)
+            return(
+                <Contact 
+                    key={contact.id}
+                    contact={contact}
+                />
+            )
+        })
         console.log(this.props.contacts)
         return(
             <div>
-                Hello from ContactsList
+                {contactsList}
             </div>
         )
     }
