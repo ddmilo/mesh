b5aa01a89a8ffd2f1d867e58b470aa6aedb8f5ce
nintichance <mintidance@gmail.com>
2018-05-04 09:24:39 -0400

    Created basic login form

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,43 @@
 import React, { Component } from 'react'
+import { Container } from './styled-components/Containers'
+import { Form } from './styled-components/Form'
 
-class Login extends Component{
+class Login extends Component {
+    state = {
+        email: '',
+        password: ''
+    }
+
+    handleChange = (event) => {
+        this.setState({ [event.target.name]: event.target.value })
+        event.preventDefault()
+    }
+
     render(){
         return(
-            <div>
-                Hello from Login
-            </div>
+            <Container>
+                <div><p> Welcome Back </p>
+                <Form onSubmit={() => this.login()}>
+                    <input 
+                        type="string"
+                        name="email"
+                        placeholder="Email"
+                        onChange={this.handleChange}
+                         />
+                    <input 
+                        type="string"
+                        name="password"
+                        placeholder="Password"
+                        onChange={this.handleChange}
+                         />
+                    <input className="input-button" type="submit" value="Login" />
+                </Form>
+                </div>
+            
+               
+            </Container>
         )
     }
 }
+
 export default Login
