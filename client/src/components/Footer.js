e9b4704a978d50c5ebc1a7f3e478c619bf3f226b
nintichance <mintidance@gmail.com>
2018-05-05 09:15:46 -0400

    Corrected filename of NewContact

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -5,7 +5,7 @@ import { Link  } from 'react-router-dom'
 const Footer = () => {
     return(
         <FooterContainer>
-            <Link to ='/new_contact'><NewContactImage src='https://i.imgur.com/5O7JcNl.png'/></Link>
+            <Link to='/new_contact'><NewContactImage src='https://i.imgur.com/5O7JcNl.png'/></Link>
         </FooterContainer>
     )
 }
