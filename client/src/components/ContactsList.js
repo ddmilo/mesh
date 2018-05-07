837d3af55dc861c729e808387f6131f5e9f614c3
nintichance <mintidance@gmail.com>
2018-05-07 04:10:54 -0400

    Uncommented Redirect in ContactsList

--- client/src/components/ContactsList.js
+++ client/src/components/ContactsList.js
@@ -34,12 +34,12 @@ class ContactsList extends Component {
     }
 
     render() {
-        // if (this.props.loggedIn === false){
-        //     alert("Log in to view your contacts")
-        //     return(
-        //         <Redirect to='/' />
-        //     )
-        // }
+        if (this.props.loggedIn === false){
+            alert("Log in to view your contacts")
+            return(
+                <Redirect to='/' />
+            )
+        }
         const orderedContacts = this.props.contacts.sort((a, b) => (a.name > b.name ? 1 : -1))
         const searchedContacts = orderedContacts.filter((contact)=>{
             if ((contact.name.toLowerCase().indexOf(this.state.search.toLowerCase()) !== -1) || (contact.relation.toLowerCase().indexOf(this.state.search.toLowerCase()) !== -1)){
